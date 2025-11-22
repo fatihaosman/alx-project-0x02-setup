e5334ed8f97eb16283889128430ecf52d402c2e2
fatihaosman <fatihaosman17@gmail.com>
2025-11-22 21:18:15 +0300

    added content in index.ts, and about.tsx

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,6 +1,6 @@
 // components/common/Button.tsx
 import React from "react";
-import { ButtonProps } from "../../interfaces";
+import { type ButtonProps } from "../../interfaces";
 
 const Button: React.FC<ButtonProps> = ({ size = "medium", shape = "rounded-md", children }) => {
   const sizeClasses = {
